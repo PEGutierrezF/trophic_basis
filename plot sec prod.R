7f9b079e09ce4709edf56e59fe5f7852bac467c4
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2023-05-30 11:05:12 -0400

    data

--- plot sec prod.R
+++ plot sec prod.R
@@ -82,3 +82,32 @@ ggplot(cloeodes_data, aes(x = periodo, y = mean,
   theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black")) #subaxis y
 
 
+
+# boxplot  ----------------------------------------------------------------
+# Create separate data frames for each taxa
+phylloicus_data <- df[df$taxa == "Phylloicus", ]
+
+# Order the levels of the "periodo" variable and change the labels
+phylloicus_data$periodo <- factor(phylloicus_data$periodo, 
+                                    levels = c("pre", "post"),
+                                    labels = c("Pre-hurricane", "Post-hurricane"))
+
+# Set up the plot
+ggplot(phylloicus_data, aes(x = periodo, y = mean, 
+                              color = periodo))+
+  geom_point(size = 8) +
+  # Add error bars for standard deviation
+  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), size=1.5, width = 0.01) +
+  # Customize appearance
+  labs(x = "Period", y = expression("Secondary Production (mg m"^-2*" mo"^-1*")")) +
+  scale_color_manual(values = c("Pre-hurricane" = "#009E73", "Post-hurricane" = "#56B4E9")) +
+  ylim(0,4) +
+  theme_bw() +
+  
+  theme(legend.position = "none") +
+  
+  theme(axis.title.x = element_text(size = 16, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 16, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=14, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=14, vjust=0.5, color="black")) #subaxis y
+
